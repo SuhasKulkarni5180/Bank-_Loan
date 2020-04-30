35df88389f0a4e59d11160439521f5b895078a3e
Suhas Kulkarni <suhas@Suhass-MacBook-Air.local>
2020-04-30 12:00:26 +0530

    Imported the dataset

---
+++ bank_loan.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 11:56:49 2020
+
+@author: suhas
+"""
+
+import pandas as pd
+import numpy as np
+
+df=pd.read_csv("/Users/suhas/Desktop/Fresh/Bank_Personal_Loan_Modelling.csv")
